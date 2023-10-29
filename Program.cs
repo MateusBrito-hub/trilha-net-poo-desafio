5c79366d1d1f95456731d596c22474829a8318f5
Brito Mateus <mateusvsbrito6@gmail.com>
2023-10-29 15:39:07 -0300

    Feat: Implementado funções de acordo com o solicitado pelo desafio, abistraindo telefone para csharp

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123", "1111111111", "Modelo 1", 32);
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+nokia.Ligar();
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("123", "22222222", "Modelo 2", 16);
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
+iphone.Ligar();
